c3121604a0fa7570e08f588cb3d48988ae699e0a
1terry <t.juottawa@gmail.com>
2021-03-26 23:00:45 -0400

    basic backend framework

--- maps.py
+++ maps.py
@@ -1,3 +1,30 @@
-import googlemaps
-from itertools import tee
+import requests
+import json
 
+# enter your api key here
+api_key ='Your_api_key'
+  
+# Take source as input
+
+source = input()
+  
+# Take destination as input
+dest = input()
+  
+# url variable store url 
+url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
+  
+# Get method of requests module
+# return response object
+r = requests.get(url + 'origins = ' + source +
+                   '&destinations = ' + dest +
+                   '&key = ' + api_key)
+                     
+# json method of response object
+# return json format result
+x = r.json()
+  
+# by default driving mode considered
+  
+# print the value of x
+print(x)
