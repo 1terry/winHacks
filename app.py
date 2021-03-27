99e15fa1f6d3a8729aef6950d457246da72bc376
1terry <t.juottawa@gmail.com>
2021-03-27 15:12:29 -0400

    Made post command

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def index():
   return render_template("index.html") 
+
+@app.route('/index', methods =["POST", "GET"])
+def getInfo:
+  if request.method == "POST"
+    loc1 = request.form["start"]    #make name start in field
+    loc2 = request.form["end"]      #make name end in field
+  
