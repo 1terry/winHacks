8b03e20330c2878f98b4542a25b40ab15a2b0f59
Jackson Xiao <jacksonxiao0@gmail.com>
2021-03-27 00:47:40 -0400

    added basic map

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
+
+load_dotenv()
+
+
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 
 @app.route('/')
 def index():
-  return render_template("index.html") 
+    key = os.environ.get("GOOGLE_MAPS_KEY")
+
+    return render_template('index.html', mapsKey=key)
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True, use_reloader=True)
