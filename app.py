ffd5729d93d6e3b50f43920f2bd29f7956b92ea0
Kevin Yang <kyang267@uwo.ca>
2021-03-27 15:57:55 -0400

    3/27/2021
    Made some changes to the backend to start a local server. Attempted to change pages.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -13,5 +13,20 @@ def getInfo():
     loc1 = request.form["start"]    #make name start in field
     loc2 = request.form["end"]      #make name end in field
     print(loc1 + loc2)
+    
+    # Pulling from the textbox and redirecting to a new screen.
+    user = request.form["nm"]
+    return redirect (url_for ("user", usr = user))
+  else:
+
+    # Otherwise we just load the start screen.
+    return render_template("index.html")
+
+# Testing to see if we can get to the user screen.
+@app.route ("/<usr>")
+def user(usr):
+  return f"<h1>{usr}</h1>"
+
+# Starting the server.
 if __name__ == "__main__":
   app.run(debug=True)
